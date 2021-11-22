9761bd75f105377744dbc4c52a1817aa1f4b9ea9
Jose Varela <joselcvarela@gmail.com>
2021-11-22 06:43:42 +0000

    add gatsby-source-graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gatsby",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Directus",
+        fieldName: "directus",
+        url: "http://localhost:8055/graphql",
+        // HTTP headers
+        headers: {
+          // Learn about environment variables: https://gatsby.dev/env-vars
+          Authorization: `Bearer varela`,
+        },
+        // Additional options to pass to node-fetch
+        fetchOptions: {},
+      },
+    },
+  ],
 };
